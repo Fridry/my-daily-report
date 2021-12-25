2918e4d3b472c9eed93bb7970cf02fc1b77f7e39
Fridry <fredwin.ed@gmail.com>
2021-12-25 15:44:03 -0300

    Add Gif and configure date

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ const EMAIL_USER = process.env.EMAIL_USER;
 const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
 const EMAIL_USER_RECIPIENT = process.env.EMAIL_USER_RECIPIENT;
 const ACCUWEATHER_API_KEY = process.env.ACCUWEATHER_API_KEY;
+const TENOR_API_KEY = process.env.TENOR_API_KEY;
 
 const fahrenheitToCelsius = (f) => {
   const temp = (f - 32) * (5 / 9);
@@ -17,60 +18,76 @@ const fahrenheitToCelsius = (f) => {
 (async function run() {
   console.log("Running report...");
 
-  const city = "Santo Ângelo, RS";
-  const countryCode = "BR";
+  try {
+    const city = "Santo Ângelo, RS";
+    const countryCode = "BR";
 
-  const locationEndpoint = `http://dataservice.accuweather.com/locations/v1/cities/${countryCode}/search`;
-  const locationRequest = await fetch(
-    `${locationEndpoint}?q=${encodeURIComponent(
-      city
-    )}&apikey=${ACCUWEATHER_API_KEY}`
-  );
+    const locationEndpoint = `http://dataservice.accuweather.com/locations/v1/cities/${countryCode}/search`;
+    const locationRequest = await fetch(
+      `${locationEndpoint}?q=${encodeURIComponent(
+        city
+      )}&apikey=${ACCUWEATHER_API_KEY}`
+    );
 
-  const locationData = await locationRequest.json();
+    const locationData = await locationRequest.json();
 
-  const locationKey = locationData[0].Key;
+    const locationKey = locationData[0].Key;
 
-  const forecastEndpoint = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}`;
-  const forecastRequest = await fetch(
-    `${forecastEndpoint}?apikey=${ACCUWEATHER_API_KEY}&language=pt-br`
-  );
+    const forecastEndpoint = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}`;
+    const forecastRequest = await fetch(
+      `${forecastEndpoint}?apikey=${ACCUWEATHER_API_KEY}&language=pt-br`
+    );
 
-  const forecastData = await forecastRequest.json();
+    const forecastData = await forecastRequest.json();
 
-  console.log(JSON.stringify(forecastData, null, 2));
+    console.log(JSON.stringify(forecastData, null, 2));
 
-  const tempMin = forecastData.DailyForecasts[0].Temperature.Minimum.Value;
-  const tempMax = forecastData.DailyForecasts[0].Temperature.Maximum.Value;
+    const tempMin = forecastData.DailyForecasts[0].Temperature.Minimum.Value;
+    const tempMax = forecastData.DailyForecasts[0].Temperature.Maximum.Value;
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: EMAIL_USER,
-      pass: EMAIL_PASSWORD,
-    },
-  });
+    const gifEndpoint = `https://g.tenor.com/v1/trending`;
+    const gifRequest = await fetch(
+      `${gifEndpoint}?contentfilter=off&limit=1&key=${TENOR_API_KEY}`
+    );
+    const gifData = await gifRequest.json();
 
-  await transporter.sendMail({
-    from: EMAIL_USER,
-    to: EMAIL_USER_RECIPIENT,
-    subject: "Daily Report",
-    text: `
-    Daily Report
+    const date = new Date();
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: EMAIL_USER,
+        pass: EMAIL_PASSWORD,
+      },
+    });
+
+    await transporter.sendMail({
+      from: EMAIL_USER,
+      to: EMAIL_USER_RECIPIENT,
+      subject: `Daily Report - ${date.toDateString()}`,
+      text: `
+    Daily Report - ${date}
 
     Weather
     - Forecast: ${forecastData.Headline.Text}
     - Temp Min: ${fahrenheitToCelsius(tempMin)} °C
     - Temp Max: ${fahrenheitToCelsius(tempMax)} °C
+
+    Daily Gif: ${gifData.results[0].url}
     `,
-    html: `
-      <h1>Daily Report</h1>
+      html: `
+      <h1>Daily Report - ${date}</h1>
       <h2>Weather</h2>
       <p>Forecast: ${forecastData.Headline.Text}</p>
       <p>Temp Min: ${fahrenheitToCelsius(tempMin)} °C</p>
       <p>Temp Max: ${fahrenheitToCelsius(tempMax)} °C</p>
+
+      <p><img src="${gifData.results[0].media[0].tinygif.url}" /></p>
     `,
-  });
+    });
+  } catch (error) {
+    console.error(error);
+  }
 })();
